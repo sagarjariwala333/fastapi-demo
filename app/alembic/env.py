7269e4166f0127b501950364d29a7135ad988bb9
Sagar <sagarjariwala333@gmail.com>
2024-07-16 14:41:45 +0530

    feat: created models and migrations for employees and tasks

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,9 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config # type: ignore
-from sqlalchemy import pool # type: ignore
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
 from alembic import context
+from models.example import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -16,9 +17,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from app.models import example
-# target_metadata = mymodel.Base.metadata
-target_metadata = example.Base.metadata
+# from myapp import mymodel
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- app/models/example.py
+++ app/models/example.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String # type: ignore
+from sqlalchemy import Column, Integer, String, ForeignKey # type: ignore
 from sqlalchemy.ext.declarative import declarative_base # type: ignore
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -7,3 +8,16 @@ class Example(Base):
     __tablename__ = "examples"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+
+class Employee(Base):
+    __tablename__ = "employees"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    tasks = relationship("Task", back_populates="employee")
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    employee_id = Column(Integer, ForeignKey("employees.id"))
+    employee = relationship("Employee", back_populates="tasks")
