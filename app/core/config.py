82dd9cb12444ee51acc434408c1682cb4f2b4b14
Sagar <sagarjariwala333@gmail.com>
2024-08-10 16:15:28 +0530

    refactor: corrected app service

--- app/core/config.py
+++ app/core/config.py
@@ -1,18 +1,14 @@
 import os
-from dotenv import load_dotenv # type: ignore
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    ...
-
-
-load_dotenv()
+from pydantic_settings import BaseSettings # type: ignore
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI Master Template"
     DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET: str = os.getenv("SECRET")
     ALGORITHM: str = os.getenv("ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES: str = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
+
+    class Config:
+        env_file = ".env"  # Automatically load from .env file if it exists
 
 settings = Settings()
